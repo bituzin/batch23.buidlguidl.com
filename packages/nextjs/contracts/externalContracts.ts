281d85549befb75fa56168aff90cc969d7175736
Egor Gusev <guy.do.or.die@gmail.com>
2026-01-16 18:25:34 +0400

    Revert the graduate contract address to batch 23

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -365,9 +365,7 @@ const externalContracts = {
       },
     },
     BatchGraduationNFT: {
-      // TODO: change to current batch address after demo
-      // address: "0x6A4Ae3a5c14e89aF4C4b6081dD71B8d3A636Ea08",
-      address: "0x01ee760bA941Da2f0b0bA8F3Dd59c745Ce8862ef",
+      address: "0x6A4Ae3a5c14e89aF4C4b6081dD71B8d3A636Ea08",
       abi: [
         {
           inputs: [
